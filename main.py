7739dabf590cdc4d564ae49362a7b2a4a76c78de
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-13 10:06:08 +0300

    matrisasion avec donnee de test

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import equation as eq
+import matrisation as mtr
 
 def main():
     # Recuperation des coefficients
-    coefficients = eq.get_equation_coefficients('data.txt')
+    coefficients = eq.get_equation_coefficients_from_file('r3data.txt')
+    # Affichage des coefficients
+    # print (coefficients)
+    # Créer la matrice à partir des coefficients
+    
+    matrix = mtr.create_matrix_from_coefficients(coefficients)
+    # if matrix is not None:
+    #     print("Matrice:\n", matrix)
+    # else:
+    #     print("Les coefficients ne permettent pas de créer une matrice R2 ou R3")
+
+# Maii call
+main()
---
+++ matrisation.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def create_matrix_from_coefficients(coeffs):
+    a, b, c, d, e, f = coeffs['a'], coeffs['b'], coeffs['c'], coeffs['d'], coeffs['e'], coeffs['f']
+    
+    # Vérifier si on doit créer une matrice R3
+    if all(v != 0 for v in [a, b, c, d, e, f]):
+        M = np.array([
+            [a, e / 2, d / 2],
+            [e / 2, b, f / 2],
+            [d / 2, f / 2, c]
+        ])
+        return M
+    
+    # Vérifier si on doit créer une matrice R2
+    elif all(v != 0 for v in [a, b, e]):
+        M = np.array([
+            [a, e / 2],
+            [e / 2, b]
+        ])
+        return M
+    
+    # Si les conditions ne sont pas remplies, retourner None
+    return None
